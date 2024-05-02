34cc0003ce09511d68e8d5e9b879c000687642e3
Diogo Campos <diogo_campos@ufms.br>
2024-05-02 09:21:22 -0400

    Criando Classe

---
+++ anatomia-classes/src/Classe.java
@@ -0,0 +1,14 @@
+public class Classe {
+    
+    public static void main (String[] args) {
+        String primeiroNome = "Diogo";
+        String segundoNome = "Campos";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
