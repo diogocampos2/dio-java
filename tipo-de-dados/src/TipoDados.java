ebb22274dfdb96290eb3cb0f867936e291fc3f78
Diogo Campos <diogo_campos@ufms.br>
2024-05-02 09:33:18 -0400

    Adicionando tipos de dados

---
+++ tipo-de-dados/src/TipoDados.java
@@ -0,0 +1,10 @@
+public class TipoDados {
+    public static void main(String[] args) throws Exception {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 2107033; // se começar com 0, deve se usar o tipo String.
+        long cpf = 98765432109L; //se começar com 0, deve se usar o tipo String.
+        float pi = 3.14F;
+        double salario = 1275.33;
+    }
+}
